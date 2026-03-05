3a42dc21d5421574e15ffa7e08186335ae583458
oron315 <oronbh7@gmail.com>
2026-03-06 01:46:29 +0200

    Add files via upload

---
+++ demod_samp2str.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2026 Oron and Kiran.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+
+import numpy as np
+from gnuradio import gr
+
+class demod_samp2str(gr.sync_block):
+    """
+    docstring for block demod_samp2str
+    """
+    def __init__(self, t=1,fs=1000, voltage=1, timeout = 0.1):
+        gr.sync_block.__init__(self,
+            name="demod_samp2str",
+            in_sig=[np.float32, ],
+            out_sig=None)
+        
+        self.pulse_count_in_symble = 3
+        self.char_len = 8
+        self.t = t
+        self.fs =fs
+        self.sample_per_pulse =round(fs*t)
+        self.samples_per_symble = self.sample_per_pulse*self.pulse_count_in_symble
+        self.samples_per_char = self.samples_per_symble*self.char_len
+        self.voltage = voltage
+        self.timeout = timeout
+        self.preamble_length = round(fs*t*6)
+        self.is_signal = False
+        self.bits = np.array([])
+        self.remainder = np.array([])
+
+    def work(self, input_items, output_items):
+        in0 = input_items[0]
+        # <+signal processing here+>
+
+        next_char = np.append(self.bits,self.string_from_enqueue(in0, self.fs, self.t))
+        if next_char != None:
+            print(next_char[0], end='')
+
+        return len(input_items[0])
+
+
+    def string_from_enqueue(self,in0,fs, t):
+
+
+        full_data = np.concatenate((self.remainder, np.array(in0)))
+        self.remainder = np.array([])
+
+        preamble_value = -1
+        signal_start_idx = None
+
+        #find signal start idx
+        if(self.is_signal == False):
+            if len(full_data) < self.preamble_length: #!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                self.remainder = full_data
+                return None
+            # down_sample_len = len(in0)
+            non_zero_indexes = np.nonzero(full_data)[0] #all possible starts to the preamble(all non zero elements)
+            for idx in non_zero_indexes:
+                if (all(round(samp) == preamble_value for samp in full_data[idx:idx+self.preamble_length-1])): #preamble fount at idx
+                    self.is_signal = True
+                    signal_start_idx = idx+self.preamble_length
+                    break
+
+            #if we didnt find the preamble we dont want the remainder to increase in size forever keep only the nessery part - preamble length
+            if signal_start_idx == None:
+                self.remainder = full_data[-self.preamble_length:]
+                return None
+        
+        else:
+            signal_start_idx = 0
+
+        if signal_start_idx != None:
+            start_signal_to_end = full_data[signal_start_idx:]
+        else:
+            start_signal_to_end = None
+            return None
+        
+        
+
+        
+        new_remainder_len = len(start_signal_to_end)%(self.samples_per_char)
+        self.remainder = start_signal_to_end[-new_remainder_len:] #update remainder
+
+
+        cropped_data = start_signal_to_end[:-new_remainder_len] #data to go throu for this iteration
+
+        indices = np.arange(0,len(cropped_data), self.sample_per_pulse)
+        #down_sampled = cropped_data[indices]
+
+        down_sampled = [((np.mean(cropped_data[i:i+self.sample_per_pulse])>0)*2-1) for i in indices] 
+
+
+        if len(down_sampled) >= self.char_len*self.pulse_count_in_symble:
+            msg = ''
+            for i in range(round(len(down_sampled)/(self.char_len*self.pulse_count_in_symble))):
+                msg = msg + self.vec2char(down_sampled[i*self.char_len*self.pulse_count_in_symble:(i+1)*self.char_len*self.pulse_count_in_symble])
+
+            return msg
+        else:
+            return None
+
+    
+    
+    def vec2char(self,vec):
+        mat_bit = np.reshape(vec, (-1,3))
+        bit_msg = np.array([], dtype=int)
+
+        for row in mat_bit:
+            if self.is_one(row):
+                bit_msg = np.append(bit_msg, 1)
+            elif self.is_zero(row):
+                bit_msg = np.append(bit_msg, 0)
+            # else:
+            #     bit_msg = np.append(bit_msg, 505)
+                
+        if len(bit_msg) != 8:
+            return ''
+
+        
+        # print("hihihhi")
+        # print(bit_msg)
+        
+        bit_msg = ''.join(map(str,bit_msg))  
+        # print(bit_msg)
+        msg = (chr(int(bit_msg,2)))
+        return msg
+
+
+    def is_one(self,row):
+        symbol_1 = [1,1,-1]
+        return (symbol_1==row.tolist())
+
+        
+    def is_zero(self,row):
+        symbol_0 = [1,-1,-1]
+        return (symbol_0==row.tolist())
+
+
+    def is_signal_end(self,sig):
+        if(np.count_nonzero(sig==0)>=self.timeout*self.fs):
+            return True
+        else:
+            return False
+
---
+++ mod_source_str2samp.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2026 Oron and Kiran.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+
+import numpy as np
+from collections import deque
+from gnuradio import gr
+
+class mod_source_str2samp(gr.sync_block):
+    """
+    docstring for block mod_source_str2samp
+    """
+    def __init__(self, t=1,fs=1000,msg="hello"):
+        gr.sync_block.__init__(self,
+            name="mod_source_str2samp",
+            in_sig=None,
+            out_sig=[np.float32, ])
+            
+        self.t = t
+        self.fs = fs
+        self.msg = msg
+        self.preamble_length = round(fs*t*6)
+        self.preamble_value = -1
+        self.samples_queue = deque([self.preamble_value]*self.preamble_length + self.enqueue_from_string(msg, fs, t).tolist())
+
+
+    def work(self, input_items, output_items):
+        out = output_items[0]
+        # <+signal processing here+>
+        #out[:] = 5
+
+        out_len = len(out[:])
+        #print(out_len)
+        for i in range(out_len):
+            try:
+                out[i] = self.samples_queue.popleft()
+            except:
+                out[i] = 0
+
+        return len(output_items[0])
+
+
+
+
+    def symbol_1(self,fs,t):
+        symbol = np.concatenate((np.ones(round(2*t*fs)), -1 * np.ones(round(t*fs))))
+        return symbol
+
+    def symbol_0(self,fs, t):
+        symbol = np.concatenate((np.ones(round(t*fs)), -1 * np.ones(round(2*t*fs))))
+        return symbol
+
+    def enqueue_from_string(self,msg, fs, t):
+        list_bits = ''.join(format(ord(char), '08b') for char in msg)
+        samples = np.array([])
+
+        for bit in list_bits:
+            if bit=='1':
+                samples = np.append(samples,self.symbol_1(fs,t))
+
+            else:
+                samples = np.append(samples,self.symbol_0(fs,t))    
+
+        return samples
+
+
